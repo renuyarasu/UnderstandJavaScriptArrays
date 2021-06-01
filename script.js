feefb783376d55427505fe893382ea685ba86dd7
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-01 13:18:15 +0530

    JavaScript: Object Cloning - Deep vs Shallow Copy

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
-// JavaScript: Array.concat()
+// JavaScript: Create a Shallow Copy of an Array with Slice || JavaScript Object Cloning || Deep vs Shallow Copy
 
-let people1 = [
-  {
-    name: "VedaGna",
+const person_1 = {
+  id: 1,
+  name: "VedaGna",
+  address: {
+      floatNo: 475
   },
-  {
-    name: "Renu",
-  },
-];
-let people2 = [
-  {
-    name: "Hindu",
-  },
-  {
-    name: "Gnapika",
-  },
-];
+};
+
+// using spead operator
+// const person_2 = {...person_1};
+
+// using assign operator
+// const person_2 = Object.assign({}, person_1)
+
+// using JSON.stringfy and JSON.parse
+const person_2 = JSON.parse(JSON.stringify(person_1))
 
 
-// people1.forEach(function(person){
-//     console.log(person.name);
-// })
 
+//change the id value of person_2
+// person_2.id = 2;
+person_2.address.floatNo =  500;
 
-people1.concat(people2).forEach(function(person){
-    console.log(person.name);
-})
+console.log(person_1);
+console.log(person_2);
