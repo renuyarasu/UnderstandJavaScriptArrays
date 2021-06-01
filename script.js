cf430485de4d5a9afef693bbd78099253febbe0d
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-01 16:02:13 +0530

    JavaScript: Array - Flat

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-// JavaScript: Array - Some
-
-const number = [10, 20, 30, 40, 50];
-const big = number.some((number) => number >= 50);
-console.log(big);
+// JavaScript: Array - Flat
+/**
+ *  const number = [10, 20, [30, [40, 50]]];
+    const newNumber = number.flat(2);// two level flat
+    const newNumber = number.flat(Infinity);
+    console.log(newNumber);
+ */
+const gaps = [1,2,,3,4,,5];
+const removeGaps = gaps.flat();
+console.log(removeGaps);
