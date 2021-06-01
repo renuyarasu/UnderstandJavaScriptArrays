a1ffa7aebec175b031efaa02914e98062cce5c07
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-01 13:27:31 +0530

    JavaScript: Array - Some

--- script.js
+++ script.js
@@ -1,27 +1,5 @@
-// JavaScript: Create a Shallow Copy of an Array with Slice || JavaScript Object Cloning || Deep vs Shallow Copy
+// JavaScript: Array - Some
 
-const person_1 = {
-  id: 1,
-  name: "VedaGna",
-  address: {
-      floatNo: 475
-  },
-};
-
-// using spead operator
-// const person_2 = {...person_1};
-
-// using assign operator
-// const person_2 = Object.assign({}, person_1)
-
-// using JSON.stringfy and JSON.parse
-const person_2 = JSON.parse(JSON.stringify(person_1))
-
-
-
-//change the id value of person_2
-// person_2.id = 2;
-person_2.address.floatNo =  500;
-
-console.log(person_1);
-console.log(person_2);
+const number = [10, 20, 30, 40, 50];
+const big = number.some((number) => number >= 50);
+console.log(big);
