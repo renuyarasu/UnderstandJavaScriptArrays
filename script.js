09ee2c3c24831fb613e6d7007400d2e32c571700
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-28 16:32:18 +0530

    Changed linguist-language HTML to JavaScript

--- script.js
+++ script.js
@@ -1,9 +1,2 @@
 // JavaScript: Array - filter
 
-const numbers = [-1,2,-3,4,-5,6];
-const positiveNumbers = numbers.filter(number => number >=0);
-const negativeNumbers = numbers.filter(number => number <=0);
-
-console.log(positiveNumbers);
-console.log(negativeNumbers);
-
